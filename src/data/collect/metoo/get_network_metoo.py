8d1f2593b387975e29973ab1617a27178ee83aa0
malteserteresa <tess_the_titan@hotmail.com>
2019-06-18 15:03:15 +0200

    Writes every 1000 tweets

--- src/data/collect/metoo/get_network_metoo.py
+++ src/data/collect/metoo/get_network_metoo.py
@@ -8,10 +8,9 @@ import pytest
 @click.option("--path_to_creds","-creds", required=True, help="path to twitter credentials")
 @click.option("--path_to_ids", "-ids",required=True, help="path to tweet ids")
 @click.option("--path_to_output", "-o",required=True, help="path to where the tweets should be written to")
-
-def main(path_to_creds, path_to_ids):
+def main(path_to_creds, path_to_ids, path_to_output):
     api = create_twitter_connection(path_to_creds)
-
+    gather_tweets(api, path_to_ids, path_to_output)
 
 def create_twitter_connection(path):
     """ Returns the connection to the twitter API. """
@@ -29,25 +28,38 @@ def create_twitter_connection(path):
 
     return api
 
-def returns_tweet(path_to_ids, path_to_output, api):
+def gather_tweets(api, path_to_ids, path_to_output):
+    """
+
+    """
     ids = pd.read_csv(path_to_ids)
 
-    tweet_list = []
-    for i, row in ids.iterrows():
-        if i < 2:
+    tweets = []
+    counter = 0
+    for n, row in ids.iterrows():
+        if n < len(ids):
             try:
                 id_of_tweet = row['id']
-                tweet_list.append(api.get_status(id_of_tweet)._json)
-                print(api.get_status(id_of_tweet)._json)
-                print(id_of_tweet)
+                tweets.append(api.get_status(id_of_tweet)._json)
+                print(f"Tweet id # {n}", id_of_tweet)
+
             except:
                 print(str(id_of_tweet) + ' tweet does not exist anymore')
 
-        if i % 100 == 0:
-            df = pd.DataFrame(tweet_list)
-            df.to_csv(path_to_output, mode="a", index = False)
-            tweet_list = []
-            print("Saved to file")
+            if n % 1000 == 0:
+                write_to_disk(tweets, n, path_to_output)
+                counter +=len(tweets)
+                print(f"{counter} tweets have been saved to file")
+                tweets = []
+
+
+def write_to_disk(tweets, counter, path_to_output):
+    """
+    """
+    df = pd.DataFrame(tweets)
+    df.to_csv(path_to_output, mode="a", index=False, header=True)
+    print("Saved to file")
+
 
 @pytest.fixture
 def api(path_to_creds):
@@ -64,9 +76,6 @@ def test_throws_exception_if_file_not_found():
 def test_creates_api(api):
     assert api is not None
 
-def test_retrieves_tweet(api):
-    response = api.get_status("926911797885657088")._json
-    assert response is not None
 
 
-
+main()

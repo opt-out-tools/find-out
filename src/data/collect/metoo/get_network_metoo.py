6023a4e3ae6434a8c2a38712d153be1fabca9449
malteserteresa <tess_the_titan@hotmail.com>
2019-06-25 16:27:38 +0200

    Me too fix finised

--- src/data/collect/metoo/get_network_metoo.py
+++ src/data/collect/metoo/get_network_metoo.py
@@ -1,21 +1,23 @@
-import tweepy
 import json
-import pandas as pd
 import click
-import pytest
 import mock
+import pandas as pd
+import pytest
+import tweepy
+
 
 @click.command()
-@click.option("--path_to_creds","-creds", required=True, help="path to twitter credentials")
-@click.option("--path_to_ids", "-ids",required=True, help="path to tweet ids")
-@click.option("--path_to_output", "-o",required=True, help="path to where the tweets should be written to")
+@click.option("--path_to_creds", "-creds", required=True, help="path to twitter credentials")
+@click.option("--path_to_ids", "-ids", required=True, help="path to tweet ids")
+@click.option("--path_to_output", "-o", required=True, help="path to where the tweets should be written to")
 def main(path_to_creds, path_to_ids, path_to_output):
-
+    """ Runs the functions that enable tweets to be gathered by id and saved to csv. """
     credentials = read_json(path_to_creds)
 
     api = create_twitter_API_connection(credentials)
 
-    gather_tweets(api, path_to_ids, path_to_output)
+    gather_tweets(api, path_to_ids, path_to_output, 256455)
+
 
 def read_json(path):
     """ Reads in the twitter credentials in json format and converts them to a dictionary. """
@@ -28,6 +30,7 @@ def read_json(path):
         raise FileNotFoundError("The path to your twitter credentials is not correct.")
 
     return creds
+
 
 def create_twitter_API_connection(creds):
     """ Returns the connection to the twitter API. """
@@ -43,15 +46,15 @@ def create_twitter_API_connection(creds):
 
     return api
 
-def gather_tweets(api, path_to_ids, path_to_output):
-    """
 
+def gather_tweets(api, path_to_ids, path_to_output, starting_id_idx=0):
+    """
     """
     ids = pd.read_csv(path_to_ids)
 
     tweets = []
     counter = 0
-    for n, row in ids.iterrows():
+    for n, row in ids.iloc[starting_id_idx:, : ].iterrows():
         if n < len(ids):
             try:
                 id_of_tweet = row['id']
@@ -65,10 +68,9 @@ def gather_tweets(api, path_to_ids, path_to_output):
                 if n == 0:
                     write_to_disk(tweets, True, path_to_output)
                 else:
-                    print("here")
                     write_to_disk(tweets, False, path_to_output)
 
-                counter +=len(tweets)
+                counter += len(tweets)
                 print(f"{counter} tweets have been saved to file")
                 tweets = []
 
@@ -76,19 +78,17 @@ def gather_tweets(api, path_to_ids, path_to_output):
 def write_to_disk(tweets, save_headers, path_to_output):
     """ Writes the dataframe to a csv file, appending further tweets with the tweet object keys as columns,"""
     tweet_object_keys = ['contributors', 'coordinates', 'entities',
-       'extended_entities', 'favorite_count', 'favorited', 'geo',
-       'id_str', 'in_reply_to_screen_name', 'in_reply_to_status_id',
-       'in_reply_to_status_id_str', 'in_reply_to_user_id',
-       'in_reply_to_user_id_str', 'is_quote_status', 'lang', 'place',
-       'possibly_sensitive', 'possibly_sensitive_appealable', 'quoted_status',
-       'quoted_status_id', 'quoted_status_id_str', 'retweet_count',
-       'retweeted', 'retweeted_status', 'source', 'truncated', 'user']
+                         'extended_entities', 'favorite_count', 'favorited', 'geo',
+                         'id_str', 'in_reply_to_screen_name', 'in_reply_to_status_id',
+                         'in_reply_to_status_id_str', 'in_reply_to_user_id',
+                         'in_reply_to_user_id_str', 'is_quote_status', 'lang', 'place',
+                         'possibly_sensitive', 'possibly_sensitive_appealable', 'quoted_status',
+                         'quoted_status_id', 'quoted_status_id_str', 'retweet_count',
+                         'retweeted', 'retweeted_status', 'source', 'truncated', 'user']
 
-    df = pd.DataFrame(tweets, columns = tweet_object_keys)
+    df = pd.DataFrame(tweets, columns=tweet_object_keys)
     df.to_csv(path_to_output, mode="a", index=False, header=save_headers)
     print("Saved to file")
-
-
 
 
 def test_read_json_returns_dict():
@@ -96,24 +96,27 @@ def test_read_json_returns_dict():
         credentials = read_json(mock.mock_open)
     assert isinstance(credentials, dict)
 
+
 def test_throws_exception_if_file_not_found():
     with pytest.raises(FileNotFoundError) as fnf:
         read_json("")
     assert str(fnf.value) == "The path to your twitter credentials is not correct."
 
+
 def test_raises_exception_if_incorrect_credentials():
     with pytest.raises(Exception) as e:
-        create_twitter_API_connection({"CONSUMER_KEY" : "key", "CONSUMER_SECRET": "secret", "ACCESS_TOKEN": "token", "ACCESS_SECRET": "access_secret" })
+        create_twitter_API_connection({"CONSUMER_KEY": "key", "CONSUMER_SECRET": "secret", "ACCESS_TOKEN": "token",
+                                       "ACCESS_SECRET": "access_secret"})
     assert str(e.value) == "Failed to send request, invalid credentials."
 
-def test_api_call():
-    with mock.patch('tweepy.api') as mocked_tweepy:
-        gather_tweets(mocked_tweepy, "","")
-        mocked_tweepy.get_status.assert_called_once()
 
-
-
-
+# TODO finish mocking/testing gather_tweets
+# def test_api_call():
+#     with mock.patch('tweepy.api') as mocked_tweepy:
+#         with mock.patch('pandas.read_csv', mock.mock_open(read_data="{'id': [12345]}")):
+#             mock.mock_open.iterrows.return_value = (0, 12345)
+#             gather_tweets(mocked_tweepy, mock.mock_open,"")
+#         mocked_tweepy.get_status.assert_called_once()
 
 
 if __name__ == "__main__":
--- src/data/preprocess/metoo/process_network_metoo.py
+++ src/data/preprocess/metoo/process_network_metoo.py
@@ -1,37 +1,104 @@
-import pytest
+import networkx as nx
 import pandas as pd
+from  src.utils.database_connection_handler import *
+import ast
+
+
+def insert_values(conn, tweets, users):
+    """
+    """
+
+    for tweets, users in zip(tweets, users):
+        print(tweets,  users)
+        tweet_id = create_tweets(conn, tweets)
+        create_users(conn, users, tweet_id)
+
+    print("Finished inserting values")
+
+def create_tweets(conn, tweet):
+    """
+    """
+    cur = conn.cursor()
+    sql = ''' INSERT INTO tweets(id, created_at, text)
+                     VALUES(?,?,?) '''
+    cur.execute(sql, tweet)
+    conn.commit()
+    return cur.lastrowid
+
+def create_users(conn, user, tweet_id):
+    """
+
+    """
+    import numpy as np
+    cur = conn.cursor()
+    sql = ''' INSERT INTO users(screen_name, description, tweets_id)
+                     VALUES(?,?,?) '''
+    user_with_fk = np.insert(user, 2, tweet_id)
+    cur.execute(sql, user_with_fk)
+    conn.commit()
 
 
 
+def select_all_users(conn):
+    """
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def create_df(df):
+    entity = df.fillna("{}")
+
+    entity = entity.apply(ast.literal_eval)
+
+    tmp = entity.values.tolist()
+
+    entity = pd.DataFrame(tmp)
+
+    return entity
 
 
+if __name__ == '__main__':
 
-@pytest.fixture()
-def faulty_df():
-    df1 = pd.DataFrame(
-        {"A": ["testA" for _ in range(0, 5)], "B": ["testB" for _ in range(0, 5)], "C": ["testC" for _ in range(0, 5)]})
-    header_row = pd.DataFrame({"A": ["A"], "B": ["C"], "C": ["B"]})
-    df2 = pd.DataFrame(
-        {"A": ["testA" for _ in range(0, 5)], "B": ["testC" for _ in range(0, 5)], "C": ["testB" for _ in range(0, 5)]})
-    df = pd.concat([df1, header_row, df2])
-    return df
+    df = pd.read_csv("/home/tcake/coding_projects/python/opt_out/fixed_metoo_tweets.csv")
 
-def find_header(df, headers):
-    for n, row in enumerate(df.iterrows()):
-        if row[1].isin(headers).all():
-            print(f"Header found at position {n}")
-            return n
-        else:
-            pass
+    some_tweets = df[["id", "created_at", "text"]]
+    t=  [tuple(row) for row in some_tweets.values]
 
-def test_finds_headers(faulty_df):
-    assert find_header(faulty_df, ["A", "B", "C"]) != 5 # 0-indexed like skiprows
-
-def write_position_of_headers():
-    position_of_headers = []
-    df = pd.read_csv("/media/tcake/My Passport/twitter/metoo_tweets.csv", chunksize=100, nrows=10999)
-    for chunk in df:
-        print(type(chunk))
+    users = create_df(df['user'])
+    some_users = users[['screen_name', 'description']]
+    u =  [tuple(row) for row in some_users.values]
 
 
-write_position_of_headers()
+    database = "metoo.db"
+
+    sql_create_tweets_table = """ CREATE TABLE IF NOT EXISTS tweets (
+                                        id integer PRIMARY KEY,
+                                        created_at text NOT NULL,
+                                        text text NOT NULL
+                                    ); """
+
+    sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
+                                    id integer PRIMARY KEY,
+                                    screen_name text NOT NULL,
+                                    description text,
+                                    tweets_id integer NOT NULL,
+                                    FOREIGN KEY (tweets_id) REFERENCES tweets (id)
+                                );"""
+
+    conn = create_connection(database)
+    if conn is not None:
+
+        create_table(conn, sql_create_tweets_table)
+
+        create_table(conn, sql_create_users_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+    insert_values(conn, t, u)
+    select_all_users(conn)
+    conn.close()

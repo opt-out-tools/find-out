a922f045fbfc97236c2a2710078219ffe580afcf
malteserteresa <tess_the_titan@hotmail.com>
2019-08-14 11:56:57 +0200

    Added preprocessing script for random forest that calculates TF-IDF and returns feature names

---
+++ src/data/preprocess/hatespeech/preprocess_rdforest_hatespeech.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def create_tfidf(corpus):
+    """Returns the tf-idf vector representations of documents.
+    Args:
+        corpus (list) : A list of strings
+
+    Returns:
+        "vectors" (dict)  : The vectors
+        "feature_names" (dict)  : The names of the features
+
+    """
+    vectorizer = TfidfVectorizer()
+    return { "vectors": vectorizer.fit_transform(corpus), "feature_names": vectorizer.get_feature_names()}
+

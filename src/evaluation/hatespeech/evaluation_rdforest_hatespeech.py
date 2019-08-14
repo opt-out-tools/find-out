cb1d7a0596c73053ad4a78ba8e6bf90a829c44c8
malteserteresa <tess_the_titan@hotmail.com>
2019-08-14 18:40:04 +0200

    Updated wordcloud to only plot misogynistic tweets

--- src/evaluation/hatespeech/evaluation_rdforest_hatespeech.py
+++ src/evaluation/hatespeech/evaluation_rdforest_hatespeech.py
@@ -6,13 +6,15 @@ from src.utils.normalize import normalize
 
 
 data = pd.read_csv("../../../data/external/hatespeech/hs_data.csv")
-data['normalized'] = data['text'].apply(normalize)
+misogyny = data[data['annotation'] == "misogynistic"]
+misogyny['normalized'] = misogyny['text'].apply(normalize)
 
-words = " ".join(sentence for sentence in data['normalized'].to_list())
+words = " ".join(sentence for sentence in misogyny['normalized'].to_list())
 
 wordcloud = WordCloud(background_color="white").generate(words)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
+plt.savefig("wordcloud.png")
 plt.axis("off")
 plt.show()

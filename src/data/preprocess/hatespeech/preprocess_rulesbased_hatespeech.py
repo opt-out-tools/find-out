0bc2d4b04cf4fc826546ef4e4721c8c1352e5202
malteserteresa <tess_the_titan@hotmail.com>
2019-08-15 16:34:51 +0200

    Initial rulesbased approach to studying tweets

---
+++ src/data/preprocess/hatespeech/preprocess_rulesbased_hatespeech.py
@@ -0,0 +1,17 @@
+import spacy
+import json
+import pandas as pd
+
+corpus = json.load(open("../../../../data/external/rapeglish/rape_threat_generator_data.json"))
+
+nlp = spacy.load("en_core_web_md")
+docs = [nlp(tweet) for tweet in corpus[1]]
+
+relations = []
+for i,doc in enumerate(docs):
+    dep_relation = " ".join(token.dep_ for token in doc)
+    relations.append(dep_relation)
+
+    print(f"Completed doc {i}")
+
+difference = relations - set(relations)

7ed2f0d2b0eb9e7761d64e6f962cabf138ca020e
malteserteresa <tess_the_titan@hotmail.com>
2019-08-15 09:38:47 +0200

    rulesbased hatespeech pos insight

--- src/data/preprocess/hatespeech/preprocess_rulesbased_hatespeech.py
+++ src/data/preprocess/hatespeech/preprocess_rulesbased_hatespeech.py
@@ -1,26 +1,22 @@
-from spacy import displacy
 import spacy
 import pandas as pd
 
-from src.utils.normalize import normalize
 
 data = pd.read_csv("../../../../data/external/hatespeech/hs_data.csv")
-misogyny = data[data['annotation'] == "misogynistic"]
-misogyny['normalized'] = misogyny['text'].apply(normalize)
+misogyny = data.loc[data.loc[:,'annotation'] == "misogynistic"]
 
 nlp = spacy.load("en_core_web_md")
-docs = [nlp(tweet) for tweet in misogyny['normalized'].to_list()]
+docs = [nlp(tweet) for tweet in misogyny['text'].sample(frac=0.1, replace=True).to_list()]
 
-displacy.render(docs[0], style='dep', jupyter=True, options={'distance': 90})
+
+# for ent in docs[0].ents:
+#     print("entities")
+#     print(ent.text, ent.label_)
 #
-# for token in doc:
-#     print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(
-#         token.text,
-#         token.idx,
-#         token.lemma_,
-#         token.is_punct,
-#         token.is_space,
-#         token.shape_,
-#         token.pos_,
-#         token.tag_
-#     ))
+#
+# for chunk in docs[0].noun_chunks:
+#     print("Noun-phrases")
+#     print(chunk.text, chunk.label_, chunk.root.text)
+
+for doc in docs[:50]:
+    print(doc.text, [(token.text, token.pos_) for token in doc])

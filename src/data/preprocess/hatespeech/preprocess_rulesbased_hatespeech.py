6a267c97034a81b54f9fb4a540cb061cd621f03b
malteserteresa <tess_the_titan@hotmail.com>
2019-08-19 08:08:55 +0200

    Update preprocess_rulesbased_hatespeech.py

--- src/data/preprocess/hatespeech/preprocess_rulesbased_hatespeech.py
+++ src/data/preprocess/hatespeech/preprocess_rulesbased_hatespeech.py
@@ -1,7 +1,7 @@
 import spacy
 import json
 
-corpus = json.load(open("../../../../data/external/rapeglish/rape_threat_generator_data.json"))
+corpus = json.load(open("../../../../data/external/hatespeech/hs_data.csv"))
 
 nlp = spacy.load("en_core_web_md")
 docs = [nlp(tweet) for tweet in corpus[1]]

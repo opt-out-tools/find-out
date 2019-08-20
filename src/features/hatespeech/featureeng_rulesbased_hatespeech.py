36d49d06b82dce3730c17d8d2a8380955839990f
malteserteresa <tess_the_titan@hotmail.com>
2019-08-20 09:19:35 +0200

    changed labeling values for rules

--- src/features/hatespeech/featureeng_rulesbased_hatespeech.py
+++ src/features/hatespeech/featureeng_rulesbased_hatespeech.py
@@ -1,6 +1,10 @@
 import re
 
 
+ABSTAIN = 0
+POSITIVE = 1
+NEGATIVE = 2
+
 def contains_slut_or_synonyms(tweet):
     """Returns 1 is the tweet contains the substring slut or any synonym and 0 if not
     Args:
@@ -9,7 +13,7 @@ def contains_slut_or_synonyms(tweet):
         1 or 0 (int)  : The label.
     """
     SLUT = r"""(slut|whore|tart|tramp|prostitute|hussy|floozy|harlot|hooker|vamp)"""
-    return 1 if re.search(SLUT, tweet) else 0
+    return POSITIVE if re.search(SLUT, tweet) else ABSTAIN
 
 
 def contains_sexualized_rapeglih_vocab(tweet):
@@ -20,7 +24,7 @@ def contains_sexualized_rapeglih_vocab(tweet):
         1 or 0 (int)  : The label.
     """
     RAPEGLISH = r"""(unrapeable slut|cocktease|2hole|two-hole|whore of babylon|cockteaser|slutbag|slag|ladyslut|skank|cum-guzzler|leg-opener|fuck-toy|fuck-toy)"""
-    return 1 if re.search(RAPEGLISH, tweet) else 0
+    return POSITIVE if re.search(RAPEGLISH, tweet) else ABSTAIN
 
 
 def contains_dick_or_synonym(tweet):
@@ -32,7 +36,7 @@ def contains_dick_or_synonym(tweet):
     """
     DICK = r"""\b(dick|cock|chode|fuckpole|knob)"""
 
-    return 1 if re.search(DICK, tweet) else 0
+    return POSITIVE if re.search(DICK, tweet) else ABSTAIN
 
 
 def contains_fuck_whore_same_sentence(tweet):
@@ -48,7 +52,7 @@ def contains_fuck_whore_same_sentence(tweet):
 
     for i,insult in enumerate(insults):
         print(f'This is {i} function')
-        yield 1 if re.search(insult, tweet) else 0
+        yield POSITIVE if re.search(insult, tweet) else ABSTAIN
 
 
 

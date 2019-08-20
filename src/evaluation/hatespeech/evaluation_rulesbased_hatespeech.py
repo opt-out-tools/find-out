98570c78d8122d3f6a8a9b2e0ddc897c4fcf2d03
malteserteresa <tess_the_titan@hotmail.com>
2019-08-20 13:46:18 +0200

    integrated generator function labeling with workflow

--- src/evaluation/hatespeech/evaluation_rulesbased_hatespeech.py
+++ src/evaluation/hatespeech/evaluation_rulesbased_hatespeech.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import sparse
-from metal.analysis import lf_summary
 from torch.utils.tensorboard import SummaryWriter
+from metal.analysis import lf_summary
 
 
 def make_Ls_matrix(data, labeling_functions):
@@ -44,11 +44,11 @@ def using_generator_function_make_Ls_matrix(data, generator_labeling_functions):
                     noisy_labels[i][j] = next(gen)
                 except StopIteration:
                     break
-    noisy_labels = noisy_labels[:, ~np.all(np.isnan(noisy_labels), axis=1)]
+    noisy_labels = noisy_labels[:, ~np.all(np.isnan(noisy_labels), axis=0)]
     return noisy_labels
 
 
-def analysis_of_weak_labeling(data, true_labels, labeling_functions, labeling_function_names):
+def analysis_of_weak_labeling(data, true_labels, labeling_functions, labeling_function_names, generator_labeling_functions):
     """Displays the summary of labeling functions.
 
     Args:
@@ -56,13 +56,16 @@ def analysis_of_weak_labeling(data, true_labels, labeling_functions, labeling_fu
         true_labels (pandas series) : the current labels of the data.
         labeling_functions (list) : a list of labeling functions to be performed on the text.
         labeling_function_names (list) : a list of the name of the labeling functions to be performed on the text.
+        generator_labeling_functions (list) :
 
     Returns:
         Displays a summary of the labeling function's success on the text.
     """
     SummaryWriter()
     labeling_function_matrix = make_Ls_matrix(data, labeling_functions)
+    generator_labeling_function_matrix = using_generator_function_make_Ls_matrix(data, generator_labeling_functions)
+    concat_matrix = np.hstack((labeling_function_matrix, generator_labeling_function_matrix))
     true_labels = np.array(true_labels)
-    print(lf_summary(sparse.csr_matrix(labeling_function_matrix), Y=true_labels, lf_names=labeling_function_names))
+    print(lf_summary(sparse.csr_matrix(concat_matrix), Y=true_labels, lf_names=labeling_function_names+[f"gen {i}" for i in range(0,generator_labeling_function_matrix.shape[1])]))
 
 

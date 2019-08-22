38abd01e16ee04943a2cc43b43d4f3f31612a91b
malteserteresa <tess_the_titan@hotmail.com>
2019-08-22 15:45:51 +0200

    Rules for identifying sexism added

--- src/evaluation/hatespeech/evaluation_rulesbased_hatespeech.py
+++ src/evaluation/hatespeech/evaluation_rulesbased_hatespeech.py
@@ -1,5 +1,4 @@
 import numpy as np
-import numpy.lib.recfunctions as rfn
 from scipy import sparse
 from torch.utils.tensorboard import SummaryWriter
 from metal.analysis import lf_summary
@@ -24,24 +23,23 @@ def make_Ls_matrix(data, labeling_functions):
     return noisy_labels
 
 
-def using_generator_function_make_Ls_matrix(data, generator_labeling_functions):
+def using_generator_function_make_Ls_matrix(data, lf):
     """Returns a labeling function matrix calculated using  generator functions.
 
     Args:
         data (pandas df) : the text to be labeled with.
-        labeling_functions (list) : a list of generator labeling functions to be performed on the text.
+        lf (list) : a generator labeling function to be performed on the text.
 
     Returns:
         noisy_labels (ndarray) : an nd numpy array of the labels for the text.
     """
 
-    names = [get_names(lf("")) for lf in generator_labeling_functions][0]
+    names = get_names(lf(""))
 
     noisy_labels = np.empty((len(data), len(names)))
 
     for i, row in data.iterrows():
-        for lf in generator_labeling_functions:
-            gen = lf(row.values[0].lower())
+        gen = lf(row.values[0].lower())
         j = 0
         while j < len(names):
             noisy_labels[i][j] = next(gen)[0]
@@ -75,8 +73,15 @@ def analysis_of_weak_labeling(data, true_labels, labeling_functions, labeling_fu
     """
     SummaryWriter()
     labeling_function_matrix = make_Ls_matrix(data, labeling_functions)
-    generator_labeling_function_matrix, names = using_generator_function_make_Ls_matrix(data, generator_labeling_functions)
-    concat_matrix = np.hstack((labeling_function_matrix, generator_labeling_function_matrix))
+
+    generator_labeling_function_matrix = np.empty((len(data), 1))
+    names = []
+    for lf in generator_labeling_functions:
+        new_matrix, new_names = using_generator_function_make_Ls_matrix(data, lf)
+        generator_labeling_function_matrix = np.hstack((generator_labeling_function_matrix, new_matrix))
+        names = names + new_names
+
+    concat_matrix = np.hstack((labeling_function_matrix, generator_labeling_function_matrix[:,1:]))
     true_labels = np.array(true_labels)
     print(lf_summary(sparse.csr_matrix(concat_matrix), Y=true_labels, lf_names=labeling_function_names + names))
 
--- src/features/hatespeech/featureeng_rulesbased_hatespeech.py
+++ src/features/hatespeech/featureeng_rulesbased_hatespeech.py
@@ -12,9 +12,48 @@ def contains_slut_or_synonyms(tweet):
     Returns:
         1 or 0 (int)  : The label.
     """
-    SLUT = r"""(slut|whore|tart|tramp|prostitute|hussy|floozy|harlot|hooker|vamp)"""
+    SLUT = r"""(slut|whore|tart|tramp|prostitute|hussy|floozy|harlot|hooker|vamp|slag|skank)"""
     return POSITIVE if re.search(SLUT, tweet) else ABSTAIN
 
+def contains_cunt_or_synonyms(tweet):
+    """Returns 1 is the tweet contains the substring cunt or any synonym and 0 if not
+    Args:
+        tweet (str) : The tweet.
+    Returns:
+        1 or 0 (int)  : The label.
+    """
+    CUNT = r"""(cunt|slit|pussy|fanny|twat|snatch|)"""
+    return POSITIVE if re.search(CUNT, tweet) else ABSTAIN
+
+def contains_feminazi(tweet):
+    """Returns 1 is the tweet contains the substring not sexist but or any synonym and 0 if not
+    Args:
+        tweet (str) : The tweet.
+    Returns:
+        1 or 0 (int)  : The label.
+    """
+    FEMINAZI = r"""feminazi"""
+    return POSITIVE if re.search(FEMINAZI, tweet) else ABSTAIN
+
+def contains_not_sexist(tweet):
+    """Returns 1 is the tweet contains the substring not sexist but or any synonym and 0 if not
+    Args:
+        tweet (str) : The tweet.
+    Returns:
+        1 or 0 (int)  : The label.
+    """
+    NOT_SEXIST = r"""not sexist"""
+    return POSITIVE if re.search(NOT_SEXIST, tweet) else ABSTAIN
+
+def contains_not_sexist_hashtag(tweet):
+    """Returns 1 is the tweet contains the substring notsexist hashtag but or any synonym and 0 if not
+    Args:
+        tweet (str) : The tweet.
+    Returns:
+        1 or 0 (int)  : The label.
+    """
+    NOT_SEXIST_HASHTAG = r"""notsexist"""
+    return POSITIVE if re.search(NOT_SEXIST_HASHTAG, tweet) else ABSTAIN
 
 def contains_sexualized_rapeglih_vocab(tweet):
     """Returns 1 is the tweet contains the substring which are sexualized nouns often used in rapeglish and 0 if not
@@ -23,7 +62,7 @@ def contains_sexualized_rapeglih_vocab(tweet):
     Returns:
         1 or 0 (int)  : The label.
     """
-    RAPEGLISH = r"""(unrapeable slut|cocktease|2hole|two-hole|whore of babylon|cockteaser|slutbag|slag|ladyslut|skank|cum-guzzler|leg-opener|fuck-toy|fuck-toy)"""
+    RAPEGLISH = r"""(unrapeable slut|cocktease|2hole|two-hole|whore of babylon|cockteaser|slutbag|slag|ladyslut|skank|cum-guzzler|leg-opener|fuck-toy|fuck-toy|carpet muncher)"""
     return POSITIVE if re.search(RAPEGLISH, tweet) else ABSTAIN
 
 
@@ -34,9 +73,26 @@ def contains_dick_or_synonym(tweet):
     Returns:
         1 or 0 (int)  : The label.
     """
-    DICK = r"""\b(dick|cock|chode|fuckpole|knob)"""
+    DICK = r"""\b(dick|cock|chode|fuckpole|knob|choad)"""
 
     return POSITIVE if re.search(DICK, tweet) else ABSTAIN
+
+def contains_bitch(tweet):
+    """Returns 1 is the tweet contains the substring or combinations of that are insultive and 0 if not
+    Args:
+        tweet (str) : The tweet.
+    Returns:
+        1 or 0 (int)  : The label.
+    """
+    adjectives = ["dumb", "fucking", "stupid", "greedy", "fuck", "dumbest"]
+    nouns = ["bitch", "cunt"]
+    insults = [rf"""^(?=.*\b{adjective}\b)(?=.*\b{noun}\b).*$""" for adjective in adjectives for noun in nouns]
+
+    ABSTAIN = 0
+    POSITIVE = 1
+
+    for i, insult in enumerate(insults):
+        yield POSITIVE if re.search(insult, tweet) else ABSTAIN, insult
 
 
 def contains_fuck_whore_same_sentence(tweet):
@@ -47,14 +103,32 @@ def contains_fuck_whore_same_sentence(tweet):
          1 or 0 (int)  : The label.
      """
     verbs = ["rape", "fuck"]
-    nouns = ["whore", "women"]
+    nouns = ["whore", "women", "slut", "girl"]
     insults = [rf"""^(?=.*\b{verb}\b)(?=.*\b{noun}\b).*$""" for verb in verbs for noun in nouns]
 
     ABSTAIN = 0
     POSITIVE = 1
 
     for i,insult in enumerate(insults):
-        print(f'This is {i} function')
+        yield POSITIVE if re.search(insult, tweet) else ABSTAIN, insult
+
+def contains_women_bad_drivers(tweet):
+    """Returns 1 is the tweet contains the substring women can't drive and 0 if not
+     Args:
+         tweet (str) : The tweet.
+     Returns:
+         1 or 0 (int)  : The label.
+     """
+    negation = ["cant", "cannot","shouldnt"]
+    nouns = ["whores?", "women", "sluts?", "girls?", "bitches?"]
+    verbs = ["drive","ref", "be president", "be in politics", "be a politician"]
+
+    insults = [rf"""^(?=.*\b{noun}\b)(?=.*\b{negate}\b)(?=.* \b{verb}\b).*$""" for verb in verbs for negate in negation for noun in nouns]
+
+    ABSTAIN = 0
+    POSITIVE = 1
+
+    for i,insult in enumerate(insults):
         yield POSITIVE if re.search(insult, tweet) else ABSTAIN, insult
 
 

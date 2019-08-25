9cd1ee8a7e9b170fd71900ca43d54abd436f881b
malteserteresa <tess_the_titan@hotmail.com>
2019-08-25 13:47:58 +0200

    Trasnfer learning from dataturks to sexism dataset investigation completed

---
+++ src/evaluation/hatespeech/evaluation_translearn_hatespeech.py
@@ -0,0 +1,29 @@
+from src.models.hatespeech.model_translearn_hatespeech import create_model
+from sklearn.metrics import f1_score, confusion_matrix
+
+def get_predictions(model, test):
+    y_pred = model.predict(test, batch_size=32)
+    y_pred = (y_pred > 0.5)
+    y_pred = y_pred.flatten()
+    y_pred = y_pred.astype(int)
+    return y_pred
+
+
+def get_f1score(model, test, y_test):
+    y_pred = get_predictions(model, test)
+
+    print("Confusion matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+    print("Marco F1:%f" % f1_score(y_test, y_pred, average='macro'))
+    print("Micro F1:%f" % f1_score(y_test, y_pred, average='micro'))
+    print("Weighted F1:%f" % f1_score(y_test, y_pred, average='weighted'))
+
+
+def evaluate_best_model(path_to_fine_tuned_model, x_test, y_test, word_embedding_matrix, vocab_size):
+    best_model = create_model(word_embedding_matrix, vocab_size)
+    best_model.load_weights(path_to_fine_tuned_model)
+    scores = best_model.evaluate(x_test, y_test, verbose=1)
+    print("%s: %.2f%%, %s: %.2f" % (best_model.metrics_names[1], scores[1] * 100, best_model.metrics_names[0], scores[0]))
+
+    get_f1score(best_model, x_test, y_test)
---
+++ src/models/hatespeech/model_translearn_hatespeech.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import keras
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+
+def create_model(word_embedding_matrix, vocab_size):
+    in_dim, out_dim = word_embedding_matrix.shape
+
+    model = keras.Sequential()
+    model.add(keras.layers.Embedding(vocab_size, 40))
+    # model.add(keras.layers.Embedding(in_dim, out_dim,
+    #                                  weights = [word_embedding_matrix],
+    #                                  input_length = 140,
+    #                                  trainable = False))
+    model.add(keras.layers.Dropout(0.1))
+    model.add(keras.layers.GlobalAveragePooling1D())
+    model.add(keras.layers.Dropout(0.1))
+    model.add(keras.layers.Dense(4, activation=tf.nn.relu))
+    model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+    # model.summary()
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    return model
+
+
+def freeze_layers(model, n_layer):
+    for layer in model.layers[0:n_layer]:
+        layer.trainable = False
+    return model
+
+
+def get_callbacks(filename):
+    callbacks = [ModelCheckpoint(filename, monitor='val_loss', save_best_only=True, mode='min'),
+                 EarlyStopping(monitor='val_loss', patience=5)]
+    return callbacks
+
+
+def train_model(path_to_model, datasets, vocab_size):
+    x_train, y_train = datasets[0], datasets[1]
+    x_dev, y_dev = datasets[2], datasets[3]
+    word_embedding_matrix = datasets[6]
+
+    model = create_model(word_embedding_matrix, vocab_size)
+    model.fit(x_train, y_train, epochs=50,
+              batch_size=32, validation_data=(x_dev, y_dev),
+              verbose=1, callbacks=get_callbacks(path_to_model))
+    print("Saved model to disk")
+
+
+def fine_tune_model(path_to_model, path_to_fine_tuned_model, datasets, vocab_size):
+    x_train, y_train = datasets[0], datasets[1]
+    x_dev, y_dev = datasets[2], datasets[3]
+    word_embedding_matrix = datasets[6]
+
+    model = create_model(word_embedding_matrix, vocab_size)
+    model.load_weights(path_to_model)
+    model = freeze_layers(model, n_layer=-1)
+
+    model.fit(x_train, y_train, epochs=50,
+              batch_size=32, validation_data=(x_dev, y_dev),
+              verbose=1, callbacks=get_callbacks(path_to_fine_tuned_model))
+    print("Saved model to disk")

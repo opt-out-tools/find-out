f0ab4606d19ac9fae63e65e70290e2e0c20a944e
malteserteresa <tess_the_titan@hotmail.com>
2019-09-04 14:52:59 +0200

    refactored tests into separate file

--- src/utils/preprocess_text_helpers.py
+++ src/utils/preprocess_text_helpers.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import re
 from spacy.lang.en import LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES
 from spacy.lemmatizer import Lemmatizer
@@ -6,7 +5,6 @@ from ekphrasis.classes.preprocessor import TextPreProcessor
 from ekphrasis.classes.tokenizer import SocialTokenizer
 
 from src.utils.stopwords_and_contractions import stopwords, contractions
-from src.utils.domain_objects_test import create_tweets_df, create_tokenized_tweets_df
 
 def contractions_unpacker(tweet):
     """ Returns the contracted words within the tweet as unpacked versions of themselves. eg. she's -> she is
@@ -28,24 +26,6 @@ def contractions_unpacker(tweet):
 
     return pattern.sub(replace, tweet)
 
-tweets = create_tweets_df()
-tokenized_tweets = create_tokenized_tweets_df()
-
-df_contractions = pd.DataFrame(
-        {"contraction": list(contractions().keys()), "unpacked": list(contractions().values())})
-
-def test_contraction_unpacking_all():
-    df_contractions['unpacked_values'] = df_contractions['contraction'].apply(lambda contra: contractions_unpacker(contra))
-    assert df_contractions['unpacked_values'].all() == df_contractions['unpacked'].all()
-
-def test_contraction_unpack_in_sentence():
-    tweets['contractions'] = tweets['text'].apply(lambda tweet: contractions_unpacker(tweet))
-    assert tweets.loc[3, 'contractions'] == "RT @baum_erik: Lol I am not surprised these 2 accounts blocked me @femfreq #FemiNazi #Gamergate &amp; @MomsAgainstWWE #ParanoidParent http://t.câ€¦"
-
-def test_contraction_unpack_case_agnostic():
-    assert contractions_unpacker("Ain't") == "am not"
-    assert contractions_unpacker("I'm") == "I am"
-
 def social_tokenizer(tweet):
     """Returns the tokenized sentence using a tokenizer specially designed for social network content,
     that can handle complex emoticons, emojis and other unstructured expressions like dates, times and more.
@@ -60,10 +40,7 @@ def social_tokenizer(tweet):
     social_tokenizer = SocialTokenizer(lowercase=False).tokenize
     return " ".join(s for s in social_tokenizer(tweet))
 
-def test_social_tokenizer():
-    assert social_tokenizer("@blanchettswhore Sunday,paul :)tweet? cele,blanchetts :) whore...?") == "@blanchettswhore Sunday , paul :) tweet ? cele , blanchetts :) whore . . . ?"
-    assert social_tokenizer("LIKEWISE 14:40@Reni__Rinse who's f****N dumb idea was it to change Thor to a girl?") == "LIKEWISE 14:40 @Reni__Rinse who ' s f****N dumb idea was it to change Thor to a girl ?"
-    assert social_tokenizer(":-3;‑]O_o 3:‑) >.<") == ":-3 ;‑] O_o 3:‑) >.<"
+
 
 def punctuation_cleaner(tweet):
     """Returns the sentence with punctuation removed. If there is elongated punctuation, this is also removed.
@@ -79,15 +56,6 @@ def punctuation_cleaner(tweet):
     print(tweet)
     return re.sub(r"\s[:,!.](?=\s)?", '', tweet)
 
-def test_removes_colon():
-    assert punctuation_cleaner(tokenized_tweets.loc[0, 'text']) == "RT @asredasmyhair Feminists take note #FemFreeFriday #WomenAgainstFeminism http://t.co/J2HqzVJ8Cx"
-
-def test_removes_fullstops():
-    assert punctuation_cleaner(tokenized_tweets.loc[2, 'text']) == "@MGTOWKnight @FactsVsOpinion cue the NAFALT in 3 2 1"
-
-def test_removes_exclamation_marks():
-    tweet = "@MGTOWKnight @FactsVsOpinion . . . cue the NAFALT in 3 . . 2 . . . 1 ! ! !"
-    assert punctuation_cleaner(tweet) == "@MGTOWKnight @FactsVsOpinion cue the NAFALT in 3 2 1"
 
 def lowercase(tweet):
     """Returns the sentence with all words in lowercase.
@@ -100,10 +68,6 @@ def lowercase(tweet):
 
     """
     return " ".join(word.lower() for word in tweet.split())
-
-
-def test_lowercase():
-    assert lowercase(tokenized_tweets.loc[2, 'text']) == "@mgtowknight @factsvsopinion . . . cue the nafalt in 3 . . 2 . . . 1 . . ."
 
 
 def normalizer(tweets):
@@ -122,31 +86,10 @@ def normalizer(tweets):
                     'time', 'date', 'hashtag'])
     return tweets.apply(lambda tweet: preprocesser.pre_process_doc(tweet))
 
-def test_normalize():
-    tweets['normalized'] = normalizer(tweets['text'])
-    assert tweets.loc[0, 'normalized'] == 'RT <user> : Feminists, take note. <hashtag> <hashtag> <url>'
-
-
 def escape_unicode(tweet):
     """Returns a tokenized tweet where the un-escaped unicode characters have been escaped."""
     return [word.encode('utf-8').decode('unicode_escape') if re.findall('\\\\\w+', word) != [] else word for word in
             tweet]
-
-def test_escape_a_acute():
-    assert escape_unicode(['\\xe1']) == ['á']
-
-
-def test_escape_a_umlaut():
-    assert escape_unicode(['\\xe4']) == ['ä']
-
-
-def test_escape_inverted_question_mark():
-    assert escape_unicode(['\\xbf']) == ['¿']
-
-
-def test_escape_sentence():
-    assert escape_unicode(['Iam\\xbf trying to \\xe4figure out \\xe1 unicode']) == [
-        'Iam¿ trying to äfigure out á unicode']
 
 
 def replace_spaces(tweet):
@@ -155,54 +98,12 @@ def replace_spaces(tweet):
     return [re.sub(pattern, " ", word) if re.findall(pattern, word) != [] else word for word in tweet]
 
 
-def test_replace_whitespace():
-    assert replace_spaces(['\\ntest']) == [' test']
-
-
-def test_replace_beginning():
-    assert replace_spaces(['\\xa0test test']) == [' test test']
-
-
-def test_replace_middle():
-    assert replace_spaces(['test\\xa0test']) == ['test test']
-
-
-def test_replace_middle_twice():
-    assert replace_spaces(['test\\xa0\\xa0test']) == ['test  test']
-
-
-def test_replace_end():
-    assert replace_spaces(['test\\xa0']) == ['test ']
-
-
-def test_replace_space():
-    assert replace_spaces(['test\\n test']) == ['test  test']
-
-
-def test_replace_two_times():
-    assert replace_spaces(['test\\n test\\n test']) == ['test  test  test']
-
-
-def test_replace_different_unicode():
-    assert replace_spaces(['test\\xc2\\xa0test\\ntest']) == ['test  test test']
 
 
 def remove_stopwords(tweet):
     """Returns a string of words with stop words removed."""
     return " ".join(word for word in tweet.split(" ") if word not in stopwords())
 
-
-def test_remove_stopwords():
-    assert remove_stopwords("the cat is king") == "cat king"
-
-
-def test_remove_basic_stopwords():
-    assert remove_stopwords("you i would it like") == ""
-
-
-def test_remove_stopwords_capitals():
-    # This does not remove capital stopwords as this is handled elsewhere in the normalization step
-    assert remove_stopwords("The cat Is king") == "The cat Is king"
 
 # TODO documents and testing
 def lemmatization(tweet, nlp):

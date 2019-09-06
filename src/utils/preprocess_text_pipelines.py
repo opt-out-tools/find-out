3871c92cb7f5ae75e26b50b97b8d06a3b4084421
malteserteresa <tess_the_titan@hotmail.com>
2019-09-06 11:12:23 +0200

    refactored text pipelines into classes

--- src/utils/preprocess_text_pipelines.py
+++ src/utils/preprocess_text_pipelines.py
@@ -1,4 +1,3 @@
-from tests.utils.domain_objects_test import create_tweets_df
 from src.utils.preprocess_text_helpers import (
     contractions_unpacker,
     tokenizer,
@@ -6,8 +5,44 @@ from src.utils.preprocess_text_helpers import (
     normalizer,
     punctuation_cleaner,
 )
+from typing import Callable, List
 
-tweets = create_tweets_df()
+class TextPipeline:
+
+    def __init__(self):
+        self._processors: List[Callable[[str], str]] = []
+
+    def register_processor(self, method: Callable[[str], str]):
+        self._processors.append(method)
+
+    def process_text(self, text):
+        for processor in self._processors:
+            text = processor(text)
+
+        return text
+
+
+def clean(dataframe):
+    """Returns cleaned text.
+
+          Args
+              df (pandas df) : the dataframe with the tweets under a column
+              labeled text.
+
+          Returns
+              df (pandas df) : the cleaned tweets under the column cleaned.
+
+    """
+    pipeline = TextPipeline()
+    pipeline.register_processor(contractions_unpacker)
+    pipeline.register_processor(tokenizer)
+    pipeline.register_processor(punctuation_cleaner)
+    pipeline.register_processor(remove_stopwords)
+
+    dataframe["cleaned"] = dataframe["text"].apply(
+        lambda tweet: pipeline.process_text(tweet)
+    )
+    return dataframe
 
 
 def normalize(dataframe):
@@ -21,96 +56,15 @@ def normalize(dataframe):
         df (pandas df) : the normalized tweets under the column normalized.
 
     """
-    dataframe["normalized"] = dataframe["text"].apply(
-        lambda tweet: contractions_unpacker(tweet)
-    )
-    dataframe["normalized"] = dataframe["normalized"].apply(
-        lambda tweet: tokenizer(tweet)
-    )
-    dataframe["normalized"] = dataframe["normalized"].apply(
-        lambda tweet: punctuation_cleaner(tweet)
-    )
-    dataframe["normalized"] = dataframe["normalized"].apply(
-        lambda tweet: remove_stopwords(tweet)
-    )
-    dataframe["normalized"] = normalizer(dataframe["normalized"])
-    return dataframe
+    pipeline = TextPipeline()
+    pipeline.register_processor(clean)
 
-
-def test_normalize():
-    assert (
-            normalize(tweets).loc[0, "normalized"]
-            == "RT <user> Feminists take note <hashtag> "
-               "<hashtag> <url>"
-    )
-    assert (
-            normalize(tweets).loc[1, "normalized"]
-            == "RT <user> Antis stop treating blocks "
-               "trophies soon feminists stop treating "
-               "blocks arguments đŸ \uf190 ¸ â ˜ • "
-               "<hashtag>"
-    )
-    assert normalize(tweets).loc[
-               2, "normalized"] == "<user> <user> cue NAFALT 3 2 1"
-    assert (
-            normalize(tweets).loc[3, "normalized"]
-            == "RT <user> Lol I surprised 2 accounts "
-               "blocked <user> <hashtag> <hashtag> & "
-               "<user> <hashtag> <url>"
-    )
-
-
-def clean(dataframe):
-    """Returns cleaned text.
-
-       Args
-           df (pandas df) : the dataframe with the tweets under a column
-           labeled text.
-
-       Returns
-           df (pandas df) : the cleaned tweets under the column cleaned.
-
-    """
     dataframe["cleaned"] = dataframe["text"].apply(
-        lambda tweet: contractions_unpacker(tweet)
+        lambda tweet: pipeline.process_text(tweet)
     )
-    dataframe["cleaned"] = dataframe["cleaned"].apply(
-        lambda tweet: tokenizer(tweet))
-    dataframe["cleaned"] = dataframe["cleaned"].apply(
-        lambda tweet: punctuation_cleaner(tweet)
-    )
-    dataframe["cleaned"] = dataframe["cleaned"].apply(
-        lambda tweet: remove_stopwords(tweet)
-    )
+    dataframe["normalized"] = normalizer(dataframe["cleaned"])
     return dataframe
 
-
-def test_clean():
-    assert (
-            clean(tweets).loc[
-                0, "cleaned"] == "RT @asredasmyhair Feminists take note "
-                                 "#FemFreeFriday #WomenAgainstFeminism "
-                                 "http://t.co/J2HqzVJ8Cx"
-    )
-    assert (
-            clean(tweets).loc[
-                1, "cleaned"] == "RT @AllstateJackie Antis stop treating "
-                                 "blocks trophies soon feminists stop "
-                                 "treating blocks arguments đŸ \uf190 ¸ â ˜ • "
-                                 ""
-                                 "#GamerGate"
-    )
-    assert (
-            clean(tweets).loc[2, "cleaned"]
-            == "@MGTOWKnight @FactsVsOpinion cue NAFALT 3 2 1"
-    )
-    assert (
-            clean(tweets).loc[
-                3, "cleaned"] == "RT @baum_erik Lol I surprised 2 accounts "
-                                 "blocked @femfreq #FemiNazi #Gamergate & "
-                                 "@MomsAgainstWWE #ParanoidParent "
-                                 "http://t.câ€¦"
-    )
 
 
 def tokenize(dataframe):
@@ -124,39 +78,13 @@ def tokenize(dataframe):
            df (pandas df) : the tokenized tweets under the column tokenized.
 
     """
+    pipeline = TextPipeline()
+    pipeline.register_processor(contractions_unpacker)
+    pipeline.register_processor(tokenizer)
+
     dataframe["tokenized"] = dataframe["text"].apply(
-        lambda tweet: contractions_unpacker(tweet)
-    )
-    dataframe["tokenized"] = dataframe["tokenized"].apply(
-        lambda tweet: tokenizer(tweet)
+        lambda tweet: pipeline.process_text(tweet)
     )
     return dataframe
 
 
-def test_tokenize():
-    assert (
-            tokenize(tweets).loc[0, "tokenized"]
-            == "RT @asredasmyhair : Feminists , take note "
-               ""
-               ". #FemFreeFriday #WomenAgainstFeminism "
-               "http://t.co/J2HqzVJ8Cx"
-    )
-    assert (
-            tokenize(tweets).loc[
-                1, "tokenized"] == "RT @AllstateJackie : Antis will stop "
-                                   "treating blocks as trophies as soon as "
-                                   "feminists stop treating blocks as "
-                                   "arguments . đŸ \uf190 ¸ â ˜ • #GamerGate"
-    )
-    assert (
-            tokenize(tweets).loc[2, "tokenized"]
-            == "@MGTOWKnight @FactsVsOpinion . . . cue "
-               "the NAFALT in 3 . . 2 . . . 1 . . ."
-    )
-    assert (
-            tokenize(tweets).loc[3, "tokenized"]
-            == "RT @baum_erik : Lol I am not surprised "
-               "these 2 accounts blocked me @femfreq "
-               "#FemiNazi #Gamergate & @MomsAgainstWWE "
-               "#ParanoidParent http://t.câ€¦"
-    )
--- tests/utils/test_preprocess_text_helpers.py
+++ tests/utils/test_preprocess_text_helpers.py
@@ -1,47 +1,89 @@
 import pandas as pd
+
+from src.utils.domain_objects_test import create_tweets_df, \
+    create_tokenized_tweets_df
 from src.utils.preprocess_text_helpers import *
 from src.utils.stopwords_and_contractions import contractions
-from tests.utils.domain_objects_test import create_tweets_df, create_tokenized_tweets_df
 
 tweets = create_tweets_df()
 tokenized_tweets = create_tokenized_tweets_df()
 
 df_contractions = pd.DataFrame(
-        {"contraction": list(contractions().keys()), "unpacked": list(contractions().values())})
+    {"contraction": list(contractions().keys()),
+     "unpacked": list(contractions().values())})
+
 
 def test_contraction_unpacking_all():
-    df_contractions['unpacked_values'] = df_contractions['contraction'].apply(lambda contra: contractions_unpacker(contra))
-    assert df_contractions['unpacked_values'].all() == df_contractions['unpacked'].all()
+    df_contractions['unpacked_values'] = df_contractions['contraction'].apply(
+        lambda contra: contractions_unpacker(contra))
+    assert df_contractions['unpacked_values'].all() == df_contractions[
+        'unpacked'].all()
+
 
 def test_contraction_unpack_in_sentence():
-    tweets['contractions'] = tweets['text'].apply(lambda tweet: contractions_unpacker(tweet))
-    assert tweets.loc[3, 'contractions'] == "RT @baum_erik: Lol I am not surprised these 2 accounts blocked me @femfreq #FemiNazi #Gamergate &amp; @MomsAgainstWWE #ParanoidParent http://t.câ€¦"
+    tweets['contractions'] = tweets['text'].apply(
+        lambda tweet: contractions_unpacker(tweet))
+    assert tweets.loc[
+               3, 'contractions'] == "RT @baum_erik: Lol I am not surprised " \
+                                     "these 2 accounts blocked me @femfreq " \
+                                     "#FemiNazi #Gamergate &amp; " \
+                                     "@MomsAgainstWWE #ParanoidParent " \
+                                     "http://t.câ€¦"
+
 
 def test_contraction_unpack_case_agnostic():
     assert contractions_unpacker("Ain't") == "am not"
     assert contractions_unpacker("I'm") == "I am"
 
+
 def test_social_tokenizer():
-    assert tokenizer("@blanchettswhore Sunday,paul :)tweet? cele,blanchetts :) whore...?") == "@blanchettswhore Sunday , paul :) tweet ? cele , blanchetts :) whore . . . ?"
-    assert tokenizer("LIKEWISE 14:40@Reni__Rinse who's f****N dumb idea was it to change Thor to a girl?") == "LIKEWISE 14:40 @Reni__Rinse who ' s f****N dumb idea was it to change Thor to a girl ?"
+    assert tokenizer(
+        "@blanchettswhore Sunday,paul :)tweet? cele,blanchetts :) "
+        "whore...?") == "@blanchettswhore Sunday , paul :) tweet ? cele , " \
+                        "blanchetts :) whore . . . ?"
+    assert tokenizer(
+        "LIKEWISE 14:40@Reni__Rinse who's f****N dumb idea was it to change "
+        "Thor to a girl?") == "LIKEWISE 14:40 @Reni__Rinse who ' s f****N " \
+                              "dumb idea was it to change Thor to a girl ?"
     assert tokenizer(":-3;‑]O_o 3:‑) >.<") == ":-3 ;‑] O_o 3:‑) >.<"
 
+
 def test_punctuation_cleaner_removes_colon():
-    assert punctuation_cleaner(tokenized_tweets.loc[0, 'text']) == "RT @asredasmyhair Feminists take note #FemFreeFriday #WomenAgainstFeminism http://t.co/J2HqzVJ8Cx"
+    assert punctuation_cleaner(tokenized_tweets.loc[
+                                   0, 'text']) == "RT @asredasmyhair " \
+                                                  "Feminists take note " \
+                                                  "#FemFreeFriday " \
+                                                  "#WomenAgainstFeminism " \
+                                                  "http://t.co/J2HqzVJ8Cx"
+
 
 def test_punctuation_cleaner_removes_fullstops():
-    assert punctuation_cleaner(tokenized_tweets.loc[2, 'text']) == "@MGTOWKnight @FactsVsOpinion cue the NAFALT in 3 2 1"
+    assert punctuation_cleaner(tokenized_tweets.loc[
+                                   2, 'text']) == "@MGTOWKnight " \
+                                                  "@FactsVsOpinion cue the " \
+                                                  "NAFALT in 3 2 1"
+
 
 def test_punctuation_cleaner_removes_exclamation_marks():
-    tweet = "@MGTOWKnight @FactsVsOpinion . . . cue the NAFALT in 3 . . 2 . . . 1 ! ! !"
-    assert punctuation_cleaner(tweet) == "@MGTOWKnight @FactsVsOpinion cue the NAFALT in 3 2 1"
+    tweet = "@MGTOWKnight @FactsVsOpinion . . . cue the NAFALT in 3 . . 2 . " \
+            ". . 1 ! ! !"
+    assert punctuation_cleaner(
+        tweet) == "@MGTOWKnight @FactsVsOpinion cue the NAFALT in 3 2 1"
+
 
 def test_lowercase():
-    assert lowercase(tokenized_tweets.loc[2, 'text']) == "@mgtowknight @factsvsopinion . . . cue the nafalt in 3 . . 2 . . . 1 . . ."
+    assert lowercase(tokenized_tweets.loc[
+                         2, 'text']) == "@mgtowknight @factsvsopinion . . . " \
+                                        "cue the nafalt in 3 . . 2 . . . 1 . " \
+                                        ". ."
+
 
 def test_normalize():
     tweets['normalized'] = normalizer(tweets['text'])
-    assert tweets.loc[0, 'normalized'] == 'RT <user> : Feminists, take note. <hashtag> <hashtag> <url>'
+    assert tweets.loc[
+               0, 'normalized'] == 'RT <user> : Feminists, take note. ' \
+                                   '<hashtag> <hashtag> <url>'
+
 
 def test_escape_a_acute():
     assert escape_unicode(['\\xe1']) == ['á']
@@ -56,8 +98,11 @@ def test_escape_inverted_question_mark():
 
 
 def test_escape_sentence():
-    assert escape_unicode(['Iam\\xbf trying to \\xe4figure out \\xe1 unicode']) == [
-        'Iam¿ trying to äfigure out á unicode']
+    assert escape_unicode(
+        ['Iam\\xbf trying to \\xe4figure out \\xe1 unicode']) == [
+               'Iam¿ trying to äfigure out á unicode']
+
+
 def test_replace_whitespace():
     assert replace_spaces(['\\ntest']) == [' test']
 
@@ -101,8 +146,3 @@ def test_remove_basic_stopwords():
 def test_remove_stopwords_capitals():
     # This does not remove capital stopwords as this is handled elsewhere in the normalization step
     assert remove_stopwords("The cat Is king") == "The cat Is king"
-
-
-
-
-

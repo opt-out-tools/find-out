87894b38827da80f80cb8617793400a9b0d3b383
MikeJohnPage <m.page.j@gmail.com>
2019-09-09 16:56:14 +0100

    add one-hot encoding

--- src/models/stanford_hatespeech/model_rnn_stanford_hatespeech.py
+++ src/models/stanford_hatespeech/model_rnn_stanford_hatespeech.py
@@ -1,2 +1,15 @@
 # one-hot encode data
 
+from keras.preprocessing.text import Tokenizer
+
+samples = # read in tabular data from hatespeech and link to abraham and join as one dataframe
+
+tokenizer = Tokenizer(num_words = 1000)
+tokenizer.fit_on_texts(samples)
+
+sequences = tokenizer.text_to_sequence(samples)
+
+one_hot_results = tokenizer.texts_to_matrix(samples, mode = 'binary')
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))

922efe88d374db9f316dca0592ad3e642ef2c2cb
Matteo Guzzo <guzzo.matteo@gmail.com>
2019-10-15 15:36:52 +0200

    DO NOT MERGE

--- src/visualization/visualize_exploratory_data_analysis.py
+++ src/visualization/visualize_exploratory_data_analysis.py
@@ -2,17 +2,19 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
-def plot_word_vectors(word_vectors, words, ax):
-    """
+def plot_word_vectors(word_vectors, words, axis):
+    """Plot a 2-D projection of the embeddings
     """
     pca = PCA(n_components=2)
     pca.fit(word_vectors)
     word_vecs_2d = pca.transform(word_vectors)
 
-    ax.scatter(word_vecs_2d[:, 0], word_vecs_2d[:, 1])
+    axis.scatter(word_vecs_2d[:, 0], word_vecs_2d[:, 1])
 
     for word, coord in zip(words, word_vecs_2d):
-        X, Y = coord
-        ax.text(X, Y, word[0], size=15)
+        x, y = coord
+        axis.text(x, y, word[0], size=15)
 
-    return ax
+    axis.plot()
+    plt.show()
+    return axis
